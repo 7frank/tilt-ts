b104b844a93ee927eb9c1aecdc9b951f0bcc424e
Frank <frank.reimann@jambit.de>
2025-02-15 17:09:01 +0100

    docker tag and push example to private registry

--- index.ts
+++ index.ts
@@ -3,27 +3,33 @@ import Docker, {
   type ImageBuildOptions,
 } from "dockerode";
 
-import { $, file, FileBlob} from "bun";
+import { finished } from "stream/promises";
+
+import { $, file, FileBlob } from "bun";
 
 const PORT = 3001;
 
 interface GlobalTiltState {
   k8s: { context: string; namespace: string };
   docker: { registry: string };
-  docker_build: Record<string,Parameters<typeof docker_build>>;
+  docker_build: Record<string, Parameters<typeof docker_build>>;
+  k8s_yaml: Record<string, Parameters<typeof k8s_yaml>>;
 }
 
 const initialTiltState: GlobalTiltState = {
   docker: { registry: "localhost:5000" },
   k8s: { context: "k3d-ecosys-local-dev  ", namespace: "eco_test" },
   docker_build: {},
+  k8s_yaml: {},
 };
 
-async function getCachedConfig(fp: FileBlob,initialTiltState: GlobalTiltState):Promise<GlobalTiltState> {
+async function getCachedConfig(
+  fp: FileBlob,
+  initialTiltState: GlobalTiltState
+): Promise<GlobalTiltState> {
   await $`mkdir -p .tilt-ts`;
 
-
-  return (await fp.exists()) ? await fp.json() : initialTiltState; 
+  return (await fp.exists()) ? await fp.json() : initialTiltState;
 }
 
 /*
@@ -42,11 +48,11 @@ type SYNC = ReturnType<typeof sync>;
 const run = (fileOrPath: string, options: { trigger: string[] }) => {
   return { type: "run", path: fileOrPath, options };
 };
-  
+
 type RUN = ReturnType<typeof run>;
 const configFile = file(`.tilt-ts/state-${PORT}.json`);
 
-const tiltState = await getCachedConfig(configFile,initialTiltState);
+const tiltState = await getCachedConfig(configFile, initialTiltState);
 
 const cloneDeep = require("clone-deep");
 
@@ -63,13 +69,17 @@ export function docker_build(
     live_update?: (SYNC | RUN)[];
   }
 ) {
-  tiltState.docker_build[imageName]=[imageName, buildContext, hot];
+  tiltState.docker_build[imageName] = [imageName, buildContext, hot];
+}
+
+export function k8s_yaml(yamlFileName: string) {
+  tiltState.k8s_yaml[yamlFileName] = [yamlFileName];
 }
 
 docker_build(
-  "ecosystem/dashie",
+  "ecosystem/nginx",
   {
-    context: ".",
+    context: "./example",
     src: ["Dockerfile"],
   },
   {
@@ -108,7 +118,6 @@ export function watchAndSyncFiles(
 
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
-
 var lhs = oldTiltState;
 
 var rhs = tiltState;
@@ -117,32 +126,51 @@ import { diff as changes, applyChange } from "deep-diff";
 var differences = changes(lhs, rhs);
 console.log(differences);
 
-
 import * as jsondiffpatch from "jsondiffpatch";
 
 const diffpatcher = jsondiffpatch.create({
-    objectHash: function (obj:any) {
-      return obj.name;
-    },
-  });
- 
+  objectHash: function (obj: any) {
+    return obj.name;
+  },
+});
+
 const delta = diffpatcher.diff(lhs, rhs);
-console.log("delta",delta)
-  
+console.log("delta", delta);
 
- 
-await configFile.write(JSON.stringify(tiltState,null,2))
+await configFile.write(JSON.stringify(tiltState, null, 2));
 
-const dryRun=true
+const dryRun = true;
 
+const { promisify } = require("util");
 
-for await (const [key,d] of Object.entries(tiltState.docker_build)) {
+const followProgress = promisify(docker.modem.followProgress).bind(
+  docker.modem
+);
+
+for await (const [key, d] of Object.entries(tiltState.docker_build)) {
   const [imageName, buildContext, hot] = d;
 
- break
-  await docker.buildImage(buildContext!, {
+  console.log("Building ", imageName);
+  const stream = await docker.buildImage(buildContext!, {
     t: imageName,
   } satisfies ImageBuildOptions);
+
+  stream.pipe(process.stdout);
+  await finished(stream);
+
+  const privateRegistry = "localhost:36269";
+  const privateTag = privateRegistry + "/" + imageName;
+
+  await docker.getImage(imageName).tag({ repo: privateTag });
+  console.log("Tagged", privateTag);
+
+  // Push the image
+  const pushStream = await docker
+    .getImage(privateTag)
+    .push({ authconfig: { serveraddress: privateRegistry } });
+
+  pushStream.pipe(process.stdout);
+  await finished(pushStream);
 
   const paths = (hot?.live_update ?? []).filter(
     (it) => it.type == "sync"
